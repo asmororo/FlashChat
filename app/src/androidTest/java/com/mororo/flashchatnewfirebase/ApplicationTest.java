fd17943b98d09ebf7162ad376bb4743da712ec7e
Anderson Mororo <asmororo@gmail.com>
2018-12-01 01:39:02 -0300

    Project Complete.

--- app/src/androidTest/java/com/mororo/flashchatnewfirebase/ApplicationTest.java
+++ app/src/androidTest/java/com/mororo/flashchatnewfirebase/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.mororo.flashchatnewfirebase;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/mororo/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/mororo/flashchatnewfirebase/RegisterActivity.java
@@ -1,10 +1,11 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.mororo.flashchatnewfirebase;
 
-import android.app.AlertDialog;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -14,9 +15,12 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -33,7 +37,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mConfirmPasswordView;
 
     // Firebase instance variables
-
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -59,7 +63,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         // TODO: Get hold of an instance of FirebaseAuth
-
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -105,7 +109,7 @@ public class RegisterActivity extends AppCompatActivity {
             focusView.requestFocus();
         } else {
             // TODO: Call create FirebaseUser() here
-
+            createFirebaseUser();
         }
     }
 
@@ -116,18 +120,46 @@ public class RegisterActivity extends AppCompatActivity {
 
     private boolean isPasswordValid(String password) {
         //TODO: Add own logic to check for a valid password (minimum 6 characters)
-        return true;
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        return confirmPassword.equals(password) && password.length() > 5;
     }
 
     // TODO: Create a Firebase user
+    private void createFirebaseUser(){
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    showErrorDialog("Registration attempt failed.");
+                } else {
+                    saveDisplayName();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 
     // TODO: Save the display name to Shared Preferences
-
+    private void saveDisplayName(){
+        String displayName = mUsernameView.getText().toString();
+        SharedPreferences prefs = getSharedPreferences(CHAT_PREFS, 0);
+        prefs.edit().putString(DISPLAY_NAME_KEY, displayName).apply();
+    }
 
     // TODO: Create an alert dialog to show in case registration failed
-
-
-
+    private void showErrorDialog(String message){
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
 }
